f964b7de8428748212125a800a612f7c399493fc
Vijay gupta <61613472+vijay2020pc@users.noreply.github.com>
2022-02-21 10:46:05 -0800

    Create 566. Reshape the Matrix.py

---
+++ leetcode-CP/Data-structures-Problems/566. Reshape the Matrix.py
@@ -0,0 +1,53 @@
+In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+
+You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
+Example 2:
+
+
+Input: mat = [[1,2],[3,4]], r = 2, c = 4
+Output: [[1,2],[3,4]]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+-1000 <= mat[i][j] <= 1000
+1 <= r, c <= 300
+
+Solution-:
+  class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
+        N = len(mat)
+        M = len(mat[0])
+        
+        if r*c != N*M:
+            return mat
+        
+        q = []
+        
+        for i in range(N):
+            for j in range(M):
+                q.append(mat[i][j])
+                
+        new_mat = [[0]*c for _ in range(r)]
+        
+        for i in range(r):
+            for j in range(c):
+                new_mat[i][j] = q.pop(0)
+                
+        return new_mat
