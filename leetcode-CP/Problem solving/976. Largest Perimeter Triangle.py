9584fa7249cc55718de20c1597ef9ae074fd307d
Vijay gupta <61613472+vijay2020pc@users.noreply.github.com>
2022-03-04 20:21:45 -0800

    Create 976. Largest Perimeter Triangle.py

---
+++ leetcode-CP/Problem solving/976. Largest Perimeter Triangle.py
@@ -0,0 +1,33 @@
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,2]
+Output: 5
+Example 2:
+
+Input: nums = [1,2,1]
+Output: 0
+ 
+
+Constraints:
+
+3 <= nums.length <= 104
+1 <= nums[i] <= 106
+
+Solution:
+  
+  class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        arr=[]
+        for i in range(len(nums)-2):
+            if nums[i]+nums[i+1]>nums[i+2]:
+                arr.append(nums[i]+nums[i+1]+nums[i+2])
+            else:
+                arr.append(0)
+        return max(arr)
+        
+        
